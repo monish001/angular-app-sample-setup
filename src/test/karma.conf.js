f317e8c27c9a307170f8a028f16b3f3a717916b2
Monish Gupta <monish.gupta@rokittech.com>
2015-10-01 16:22:06 +0530

    Setup the unit test reporters growl and coverage with grunt

--- src/test/karma.conf.js
+++ src/test/karma.conf.js
@@ -41,14 +41,25 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'app/scripts/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['progress', 'growl', 'coverage'],
+	growlReporter:{
+		prefix:'UNIT TESTS-'
+	},
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 8080,
@@ -75,7 +86,9 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       "karma-phantomjs-launcher",
-      "karma-jasmine"
+      "karma-jasmine",
+	  "karma-coverage",
+	  "karma-growl-reporter"
     ],
 
 
