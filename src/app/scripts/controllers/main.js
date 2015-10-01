b7dab6e52dc58dcbcfd4c0858123d821f3599752
Monish Gupta <monish.gupta@rokittech.com>
2015-10-01 15:53:24 +0530

    Setup using yeoman

---
+++ src/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularAppSampleSetupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularAppSampleSetupApp
+ */
+angular.module('angularAppSampleSetupApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
